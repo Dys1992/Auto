e4f6999cdc2ccf63ed27f4dda145b404a696f065
Dys <13962468020@163.com>
2018-01-27 13:22:32 +0800

    fy39919

--- src/main/java/model/flightrequest/FlightInfo.java
+++ src/main/java/model/flightrequest/FlightInfo.java
@@ -1,4 +1,51 @@
 package model.flightrequest;
 
 public class FlightInfo {
+
+    private String depCode;
+    private String arrCode;
+    private String userIp;
+    private String flat;
+    private String productType;
+
+
+    public String getDepCode() {
+        return depCode;
+    }
+
+    public void setDepCode(String depCode) {
+        this.depCode = depCode;
+    }
+
+    public String getArrCode() {
+        return arrCode;
+    }
+
+    public void setArrCode(String arrCode) {
+        this.arrCode = arrCode;
+    }
+
+    public String getUserIp() {
+        return userIp;
+    }
+
+    public void setUserIp(String userIp) {
+        this.userIp = userIp;
+    }
+
+    public String getFlat() {
+        return flat;
+    }
+
+    public void setFlat(String flat) {
+        this.flat = flat;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
 }
