67d1de1882f6102c935b5bcfd63440cd3dfab451
fy39919 <352871913@qq.com>
2018-02-22 15:51:40 +0800

    fy39919

--- src/main/java/constants/FilePathConstants.java
+++ src/main/java/constants/FilePathConstants.java
@@ -6,4 +6,7 @@ public class FilePathConstants {
     public static final String excelFilePath = "./src/datatemples/TFlightSearchData.xls";
 
     public static final String redisAddress = "127.0.0.1";
+    public static final int flyOffTime = 0;
+
+
 }
