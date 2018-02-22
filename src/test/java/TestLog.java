6772b3652030cdfd668809502301b08b36f8ac8d
fy39919 <352871913@qq.com>
2018-02-22 15:53:00 +0800

    fy39919

--- src/test/java/TestLog.java
+++ src/test/java/TestLog.java
@@ -1,15 +1,14 @@
-import common.SaveSearchResponse;
 import redis.clients.jedis.Jedis;
 
 public class TestLog {
 
     public static void main(String[] args) {
-//        Jedis jedis = new Jedis("127.0.0.1");
-//        System.out.println(jedis.get("wxSHAPEK"));
+        Jedis jedis = new Jedis("127.0.0.1");
+        System.out.println(jedis.get("wxSHAPEK"));
 
 
-        SaveSearchResponse saveSearchResponse = new SaveSearchResponse();
-        saveSearchResponse.saveResponse("wx");
+//        SaveSearchResponse saveSearchResponse = new SaveSearchResponse();
+//        saveSearchResponse.saveResponse("wx");
     }
 
 }
--- src/test/java/TestSaveData.java
+++ src/test/java/TestSaveData.java
@@ -1,10 +1,15 @@
-import common.DateUtil;
+import common.SaveSearchResponse;
 
 import java.io.IOException;
 import java.text.ParseException;
 
 public class TestSaveData {
     public static void main(String[] args) throws IOException, ParseException {
-        System.out.println(DateUtil.getNoSecondTime());
+        SaveSearchResponse saveSearchResponse = new SaveSearchResponse();
+        saveSearchResponse.saveResponse("wx");
+
+//
+//        ExcelUtil.getExcelData("wx");
+
     }
 }
